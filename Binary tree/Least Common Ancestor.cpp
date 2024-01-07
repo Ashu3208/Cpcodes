d1dfd61d8bd5ec97b76588f2329406df9d03b27b
Ashutosh Agrawal <92543518+Ashu3208@users.noreply.github.com>
2024-01-07 18:28:43 +0530

    Create Least Common Ancestor.cpp

---
+++ Binary tree/Least Common Ancestor.cpp
@@ -0,0 +1,56 @@
+/**
+Find the lowest common ancestor in an unordered binary tree given two values in the tree.
+
+Lowest common ancestor: the lowest common ancestor (LCA) of two nodes v and w in a tree or directed acyclic graph (DAG) is the lowest (i.e. deepest) node that has both v and w as descendants.
+
+Problem Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+Soution Approach : Store the path from the root to n1 and root to n2 in two separate data structures. 
+                  Then look simultaneously into the values stored in the data structure, and look for the first mismatch.
+**/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ bool findPath(TreeNode* root, vector<int>& path, int k)
+{
+    // base case
+    if (root == NULL)
+        return false;
+ 
+    // Store this node in path vector. The node will be
+    // removed if not in path from root to k
+    path.push_back(root->val);
+ 
+    // See if the k is same as root's key
+    if (root->val == k)
+        return true;
+ 
+    // Check if k is found in left or right sub-tree
+    if ((root->left && findPath(root->left, path, k))
+        || (root->right && findPath(root->right, path, k)))
+        return true;
+ 
+    // If not present in subtree rooted with root, remove
+    // root from path[] and return false
+    path.pop_back();
+    return false;
+}
+int Solution::lca(TreeNode* A, int B, int C) {
+    vector<int> path1, path2;
+
+    if (!findPath(A, path1, B)
+        || !findPath(A, path2, C))
+        return -1;
+    int i;
+    for (i = 0; i < path1.size() && i < path2.size(); i++)
+        if (path1[i] != path2[i])
+            break;
+    return path1[i - 1];
+}
